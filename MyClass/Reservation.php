dbae4547860bc015ac94b9184a377febdc391d36
demostheneromane <demostheneromane@yahoo.fr>
2023-01-22 20:01:40 -0300

    Constantes

---
+++ MyClass/Reservation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MyClass; 
+
+class Reservation 
+{
+  const APPROVAL_PENDING = 'en attente'; 
+  const APPROVAL_APPROVED = 'approuvée';
+  const APPROVAL_REJECTED = 'rejetée';
+}
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+use MyClass\Reservation; 
+
+require '../vendor/autoload.php';
+
+echo Reservation::APPROVAL_PENDING;
+
+?>
